9ad31ccb6ffdbfac2943393d2afa209113f332d6
Barbatronic <adrien.bracq@gmail.com>
2023-05-09 23:33:24 +0200

    add neopixel

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,21 @@
 #include <Arduino.h>
 #include <Servo.h>
+#include <Adafruit_NeoPixel.h>
+
+#define PIN 9
 
 const int pinServo  = 11;
 const int pinLed    = 13;
 const int pinSensor = 10;
 
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(7, PIN, NEO_GRB + NEO_KHZ800);
 
 const int timeout = 5000;
 int elapsedTime = 0;
 
 Servo elevator ;
+
+void setColor(uint32_t c);
 
 void setup() 
 {
@@ -18,6 +24,11 @@ void setup()
 
   digitalWrite(pinLed,HIGH);
 
+  strip.begin();
+  strip.setBrightness(50);
+  strip.show();
+
+  setColor(strip.Color(0, 0, 255));
   elevator.attach(pinServo);
   elevator.write(120);
   delay(2000);
@@ -25,17 +36,20 @@ void setup()
   elevator.detach();
   delay(2000);
   elapsedTime = millis();
+  setColor(strip.Color(0, 255, 0));
 }
 
 void loop() 
 {
   if(!digitalRead(pinSensor))
   {
+    setColor(strip.Color(255, 0, 0));
     elevator.attach(pinServo);
     elevator.write(120);
     delay(5000);
     elevator.writeMicroseconds(1000);
     elevator.detach();
+    setColor(strip.Color(0, 255, 0));
   }
   /*
   if(millis()-elapsedTime > timeout)
@@ -49,4 +63,15 @@ void loop()
     elapsedTime = millis();
   }
   */
+}
+
+void setColor(uint32_t c) {
+  uint16_t i, j;
+
+  for(j=0; j<256; j++) {
+    for(i=0; i<strip.numPixels(); i++) {
+      strip.setPixelColor(i, c);
+    }
+    strip.show();
+  }
 }
